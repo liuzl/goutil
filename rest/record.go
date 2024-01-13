ed6effef6647858cdfe42962689881cfec5425af
liuzl <liang@zliu.org>
2024-01-13 18:02:22 +0800

    Add duration tracking to Record function

--- rest/record.go
+++ rest/record.go
@@ -4,15 +4,17 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httputil"
+	"time"
 
 	"github.com/carlmjohnson/requests"
 )
 
-func Record(rt http.RoundTripper, rawReq *string, rawRes *string) requests.Transport {
+func Record(rt http.RoundTripper, rawReq *string, rawRes *string, duration *time.Duration) requests.Transport {
 	if rt == nil {
 		rt = http.DefaultTransport
 	}
 	return requests.RoundTripFunc(func(req *http.Request) (res *http.Response, err error) {
+		start := time.Now()
 		defer func() {
 			if err != nil {
 				err = fmt.Errorf("problem while recording transport: %w", err)
@@ -27,6 +29,7 @@ func Record(rt http.RoundTripper, rawReq *string, rawRes *string) requests.Trans
 		if res, err = rt.RoundTrip(req); err != nil {
 			return
 		}
+		*duration = time.Since(start)
 		b, err = httputil.DumpResponse(res, true)
 		if err != nil {
 			return nil, err
